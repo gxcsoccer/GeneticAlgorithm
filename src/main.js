4f05518f56ea19f79792a82835607494fa275168
gaoxiaochen <gxcsoccer@126.com>
2013-07-20 14:25:04 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame,
 	pop = [],
-	offsetX = 100,
-	offsetY = 100,
+	offsetX = 0,
+	offsetY = 0,
 	width = 200,
 	height = 200,
 	hiddenCanvas = document.createElement('canvas'),
 	hiddenContext = hiddenCanvas.getContext('2d'),
+	img = new Image(),
+	splice = Array.prototype.splice,
 	canvas, context;
 
+img.src = 'img/Firefox.png';
 
+/**
+ * 颜色
+ */
+var Color = function(r, g, b, a) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+
+Color.prototype.toString = function() {
+	return 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')';
+}
+
+/**
+ * 三角形
+ */
 var Triangle = function(p1, p2, p3, color) {
 		this.p1 = p1;
 		this.p2 = p2;
@@ -68,6 +88,9 @@ Triangle.createTriangle = function() {
 };
 
 
+/**
+ * 单体
+ */
 var GENE_SIZE = 50;
 
 var Graph = function() {
@@ -77,38 +100,132 @@ var Graph = function() {
 			this.collection.push(Triangle.createTriangle());
 		}
 
-		hiddenContext.clearRect(0, 0, width, height);
-		this.collection.forEach(function(triangle) {
-			triangle.draw(hiddenContext);
-		});
-
-		this.fitness = 0;
+		this.calculateFitness();
 	};
 
 Graph.prototype = {
 	mutation: function() {
 		var pos = Math.round(Math.random() * (GENE_SIZE - 1));
 		this.collection[pos] = Triangle.createTriangle();
+	},
+	draw: function(context) {
+		context.clearRect(0, 0, width, height);
+		this.collection.forEach(function(triangle) {
+			triangle.draw(context);
+		});
+	},
+	calculateFitness: function() {
+		this.draw(hiddenContext);
+
+		var that = this;
+		resemble(img).compareTo(hiddenCanvas).onComplete(function(data) {
+			that.fitness = (100 - data.misMatchPercentage) / 100;
+		});
 	}
 }
 
+/**
+ * 遗传算法
+ */
+var GeneticAlgorithm = function(popSize, Pc, Pm) {
+		this.population = [];
 
+		var idv;
+		for (var i = 0; i < popSize; i++) {
+			idv = new Graph();
+			//console.log(idv.fitness)
+			this.population.push(idv);
+		}
+		this.popSize = popSize;
+		this.Pc = Pc || 1;
+		this.Pm = Pm || 0.0001;
+	}
+
+
+GeneticAlgorithm.prototype = {
+	go: function() {
+		var N = 0,
+			newPop, i, r;
+
+		for (i = 0; i < this.popSize; i++) {
+			if (this.population[i].fitness >= 0.8) {
+				console.log('bingo!' + this.population[i].toString());
+				this.population[i].draw(context);
+				return;
+			}
+		}
+
+		do {
+			this.population.sort(function(a, b) {
+				return a.fitness === b.fitness ? 0 : a.fitness < b.fitness ? 1 : -1;
+			});
+
+			console.log(this.population[0].fitness);
+			//this.population[0].draw(context);
+
+			newPop = [this.population[0], this.population[1]];
+			for (i = 2; i < this.popSize; i++) {
+				newPop.push(this.rouletteWheelSelect());
+			}
+
+			for (i = 2; i < this.popSize; i += 2) {
+				r = Math.random();
+				if (r <= this.Pc) {
+					this.crossover(newPop[i], newPop[i + 1]);
+				}
+			}
+
+			for (i = 2; i < this.popSize; i++) {
+				r = Math.random();
+				if (r <= this.Pm) {
+					newPop[i].mutation();
+				}
+			}
+
+			for (i = 0; i < this.popSize; i++) {
+				newPop[i].calculateFitness();
+				if (newPop[i].fitness >= 0.8) {
+					console.log('bingo at ' + N);
+					this.population[i].draw(context);
+					return;
+				}
+			}
+
+			this.population = newPop;
+			N++;
+		} while (N < 1000);
+	},
+	rouletteWheelSelect: function() {
+		var m = 0,
+			r = Math.random();
+		for (var i = 0; i < this.popSize; i++) {
+			/* 产生的随机数在m~m+P[i]间则认为选中了i
+			 *  因此i被选中的概率是P[i]
+			 */
+			m = m + this.population[i].fitness;
+			if (r <= m) return this.population[i];
+		}
+	},
+	crossover: function(i1, i2) {
+		var pos = Math.round(Math.random() * (GENE_SIZE - 1)),
+			diff = splice.apply(i1.collection, [pos, GENE_SIZE - pos].concat(i2.collection.slice(pos)));
+		splice.apply(i2.collection, [pos, GENE_SIZE - pos].concat(diff));
+	}
+}
+
+////////////////////////////////////////////////////////////////////
 window.onload = function() {
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
 
-	for (var i = 0; i < 50; i++) {
-		pop.push(Triangle.createTriangle());
-	}
-
-	draw();
+	var ga = new GeneticAlgorithm(10);
+	ga.go();
 };
 
 
-function draw() {
-	pop.forEach(function(triangle) {
-		triangle.draw(context);
-	});
-
-	requestAnimationFrame(draw);
-}
+// function draw() {
+// 	pop.forEach(function(triangle) {
+// 		triangle.draw(context);
+// 	});
+// 	requestAnimationFrame(draw);
+// }
